912f182f4018bab6f6739c4749e83c7bef6c7885
jakub-krejci <krejci.jakub12@gmail.com>
2026-02-11 13:37:30 +0100

    Refactor Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from "@supabase/supabase-js";
 
-export const SUPABASE_URL = 'https://eorirajluoadqhtjvboz.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_Y51Oa_SqZagdJhEgP6KQCg_4ux8L4Yt';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// ❗ Frontend = pouze Anon key / Publishable key
+export const supabase = createClient(
+  "https://eorirajluoadqhtjvboz.supabase.co", // Supabase URL
+  "sb_publishable_Y51Oa_SqZagdJhEgP6KQCg_4ux8L4Yt"       // Anon / Publishable key
+);
